164a1ba3c8993af6e359a318bd46a2a5946677f6
ASHUTOSH-309 <ashutosh.sharma83020@gmail.com>
2024-07-10 23:48:06 +0530

    database functions findByID,create

--- index.js
+++ index.js
@@ -2,38 +2,101 @@ const express = require("express");
 const fs = require("fs");
 let users = require("./MOCK_DATA.json");
 const app = express();
+const mongoose = require("mongoose");
+const { type } = require("os");
+
 const PORT = 8000;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/youtube-app")
+  .then(() => {
+    console.log("mongodb conneceted Succesfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  jobTitle: {
+    type: String,
+  },
+  gender: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("user", userSchema);
 
 app.use(express.urlencoded({ extended: false }));
 
-//REST API POINTS WHICH WILL RETURN
-app.get("/api/users", (req, res) => {
-  return res.json(users);
-});
+        //REST API POINTS WHICH WILL RETURN
+          app.get("/api/users",async  (req, res) => {
+              const allDbusers=await User.find({})
+                
 
-app.get("/users", (req, res) => {
+                 return res.json(allDbusers);
+          });
+
+
+
+
+app.get("/users",async (req, res) => {
+
+  const allDbusers=await User.find({})
+
   const html = `<ul>
-    ${users.map((user) => `<li> ${user.email} </li>`).join("")}
-    </ul>
-    `;
+                    ${allDbusers.map((user) => `<li> ${user.firstName}--${user.email} </li>`).join("")}
+               </ul>`;
   res.send(html);
 });
 
-app.post("/api/users", (req, res) => {
-  // TODO------> CREATE A NEW USER
-  return res.json({ status: "pending" });
+
+
+app.post("/api/users", async (req, res) => {
+  const body=req.body;
+
+
+  if(!body.email || !body.first_Name || !body.last_Name || !body.job_title || !body.gender){
+      return res.status(400).json({status:"All fields are requied"})
+
+  }
+
+
+  const result=await User.create({
+
+    firstName:body.first_Name,
+    lastName:body.last_Name,
+    email:body.email,
+    jobTitle:body.job_title,
+    gender:body.gender
+
+  })
+console.log(result)
+  return res.status(201).json({ msg: "success" });
 });
+
+
 
 app.patch("/api/users/:id", (req, res) => {
   const id = Number(req.params.id);
 
-    const body=req.body;
-for(var i=0;i<users.length;i++){
-if(id-1===i)
-    users[i]={...body,id:id}
-
-
-}
+  const body = req.body;
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].id === id) users[i] = { ...body, id: id };
+  }
 
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
     if (err) console.log(err);
@@ -43,42 +106,40 @@ if(id-1===i)
 });
 
 app.delete("/api/users/:id", (req, res) => {
-
-
-    //req.paramas.id always returns a string which is required to be explicitly typecasted
-    const id=Number(req.params.id);
-    let userFound=0;
-    for(var i=0;i<users.length;i++){
-
-        if(users[i].id===id){
-            userFound=1
-            users.splice(i,1);
-            fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err)=>{
-                if(err){
-                    console.log(err)
-                    return res.json({status:"error"})
-                } 
-            
-            })
-            
-            break;
-            
+  //req.paramas.id always returns a string which is required to be explicitly typecasted
+  const id = Number(req.params.id);
+  let userFound = 0;
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].id === id) {
+      userFound = 1;
+      users.splice(i, 1);
+      fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+        if (err) {
+          console.log(err);
+          return res.json({ status: "error" });
         }
+      });
 
+      break;
     }
-    
-    if(!userFound) return res.json({status:"user Not found"})
-    
-        return res.json({status:"User Succesfully deleted"})
+  }
 
+  if (!userFound) return res.json({ status: "user Not found" });
+
+  return res.json({ status: "User Succesfully deleted" });
 });
 
-app.get("/api/users/:id", (req, res) => {
+app.get("/api/users/:id",async (req, res) => {
   //req.params.id will return a string
-  console.log(typeof req.params.id);
-  const id = Number(req.params.id);
-  const myUser = users.find((user) => user.id === id);
-  return res.json(myUser);
+ 
+
+
+  const user=await User.findById(req.params.id);
+  if(!user) res.status(404).json({error:"No user with the given id exist"})
+  
+    return res.json(user);
+
+
 });
 
 app.listen(PORT, () => {
